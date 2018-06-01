bda35d310d9ba1897feb75861e13f1051d84a504
idoking211 <38375409+idoking211@users.noreply.github.com>
2018-06-01 10:40:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.json");
 const color = require("./color.json");
 const Discord = require("discord.js");
+
 const bot = new Discord.Client({disableEveryone: true});
 
 const swearWords = ["fuck", "shit", "זונה", "חרא"];
